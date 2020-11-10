1c37e7c34c70bcdcab81d4b53d94f9c8530a54d2
Maxim Petrov <mmrmaximuzz@gmail.com>
2020-11-10 10:45:30 +0300

    Move the example into unreliable.h

--- main.c
+++ main.c
@@ -1,42 +1,10 @@
-#include <error.h>
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-#include <sys/socket.h>
-#include <sys/un.h>
-#include <unistd.h>
+#include "unreliable.h"
 
 #define UNIX_SOCKET_PATHNAME "test"
 
 int main(void)
 {
-	unlink(UNIX_SOCKET_PATHNAME);
-
-	int sock = socket(AF_UNIX, SOCK_STREAM, 0);
-	if (sock == -1) {
-		perror("cannot open unix socket");
-		return EXIT_FAILURE;
-	}
-
-	struct sockaddr_un name = {
-		.sun_family = AF_UNIX,
-		.sun_path   = UNIX_SOCKET_PATHNAME,
-	};
-
-	int bindres = bind(sock,
-			   (const struct sockaddr *)&name,
-			   sizeof(struct sockaddr_un));
-	if (bindres == -1) {
-		perror("cannot bind socket");
-		return EXIT_SUCCESS;
-	}
-
-	int listenres = listen(sock, 1);
-	if (listenres == -1) {
-		perror("cannot listen on socket");
-		return EXIT_FAILURE;
-	}
+	int sock = create_unix_socket(UNIX_SOCKET_PATHNAME);
 
 	/* Wait for incoming connection. */
 	int dsock = accept(sock, NULL, NULL);
---
+++ unreliable.h
@@ -0,0 +1,64 @@
+/*
+ * Simple seccomp example for linux. In this example the unix socket is created,
+ * then it accepts connection and goes into the sandbox mode.
+ *
+ * Do not use this example as a real sandbox because it is _just_ an example.
+ * For the real sandboxes limiting the set of available system calls is not
+ * enough - they should also protect the system against the side-channel
+ * attacks.
+ */
+
+#include <error.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+
+/*
+ * Simple helper which is used to create a named unix streamsocket listening for
+ * exactly one connection at a time and bind it to @path in the filesystem.
+ *
+ * Returns socket fd on success and -1 on failures.
+ */
+static inline int create_unix_socket(const char *path)
+{
+	int err = 0;
+	int sock = -1;
+	struct sockaddr_un name = { .sun_family = AF_UNIX };
+
+	/* Check that path is not too long for `sockaddr` struct */
+	if (strlen(path) >= sizeof(name.sun_path)) {
+		fprintf(stderr, "unix socket: path is too long: %s", path);
+		goto fail;
+	}
+	strncpy(name.sun_path, path, sizeof(name.sun_path) - 1);
+
+	sock = socket(AF_UNIX, SOCK_STREAM, 0);
+	if (sock == -1) {
+		perror("cannot open unix socket");
+		goto fail;
+	}
+
+	unlink(path);
+	err = bind(sock, (struct sockaddr *) &name, sizeof(name));
+	if (err == -1) {
+		perror("cannot bind socket");
+		goto fail_sock;
+	}
+
+	err = listen(sock, 1);
+	if (err == -1) {
+		perror("cannot listen on socket");
+		goto fail_sock;
+	}
+
+	return sock;
+
+fail_sock:
+	close(sock);
+fail:
+	return -1;
+}
